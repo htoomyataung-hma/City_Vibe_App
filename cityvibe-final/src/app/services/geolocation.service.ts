faa3aa744992f7810b2fd5c21a49515d8385e54c
Htoo Myat Aung <htoomyataung039@gmail.com>
2025-08-06 13:57:13 +0100

    delete all file

--- cityvibe-final/src/app/services/geolocation.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-import { Geolocation, Position } from '@capacitor/geolocation';
-@Injectable({ providedIn: 'root' })
-export class GeolocationService {
-    getCurrentPosition(): Promise<Position> {
-        return Geolocation.getCurrentPosition({ enableHighAccuracy: true });
-    }
-}
